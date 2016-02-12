f76c80f4a9cf150ba32feed4f7f2d6d3617a488c
enmiller94 <millerev17@hanover.edu>
2016-02-12 08:32:03 -0500

    Wrote tests for func_of_calc.

--- assignment5tests.ml
+++ assignment5tests.ml
@@ -42,6 +42,18 @@ let t10l = calc_eval (Int 2, 3) = 2
 let t10m = calc_eval (Var, 3) = 3
 
 let t11a = func_of_calc (Add (Var, Int 2)) 3 = 5
+let t11b = func_of_calc (Add (Int 2, Int 2)) 3 = 4
+let t11c = func_of_calc (Add (Var, Var)) 3 = 6
+let t11d = func_of_calc (Mul (Var, Int 2)) 3 = 6
+let t11e = func_of_calc (Mul (Int 2, Int 2)) 3 = 4
+let t11f = func_of_calc (Mul (Var, Var)) 3 = 9
+let t11g = func_of_calc (Sub (Var, Int 2)) 3 = 1
+let t11h = func_of_calc (Sub (Int 2, Int 2)) 3 = 0
+let t11i = func_of_calc (Sub (Var, Var)) 3 = 0
+let t11j = func_of_calc (Parity (Var)) 3 = 1
+let t11k = func_of_calc (Parity (Int 2)) 3 = 0
+let t11l = func_of_calc (Int 2) 3 = 2
+let t11m = func_of_calc (Var) 3 = 3
 
 let t12a = subst (Add (Var, Int 1), Mul (Var, Var)) =
                 Mul (Add (Var, Int 1), Add (Var, Int 1))
