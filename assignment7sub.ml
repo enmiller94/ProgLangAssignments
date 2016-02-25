1b53e623c20bf9b28366f5d59cc559cc882054c9
Evan Miller <Evan Miller>
2016-02-25 15:27:39 -0500

    Wrote function for string_of_pxl. Close #180

--- assignment7sub.ml
+++ assignment7sub.ml
@@ -66,16 +66,129 @@ let base = [
 (*
    These two functions provided to you. Study how they work before continuing!
 *)
+(*
+You are provided with a function `valid_pic` that is given a picture and 
+returns a boolean indicating whether the picture is "valid", in the sense 
+of having all rows of equal length. Every function that follows assumes that 
+it is provided a valid picture. Study this function, and the next.
+*)
 let valid_pic pic =
    match List.map List.length pic with
    | [] -> true
    | x :: xs -> List.for_all ((=) x) xs
 
-let dims_pic pic =
-   match pic with
-   | [] -> (0, 0)
-   | row :: _ -> (List.length pic, List.length row)
 
 (*
-   Add your other functions here
+You are provided a function `dims_pic` that is given a (valid) picture and 
+returns a pair of the number of rows and number of columns of the picture.
+*)
+let dims_pic pic = 
+   match pic with 
+   | [] -> (0, 0) 
+   | row :: _ -> (List.length pic, List.length row) 
+
+(*
+Write a function `string_of_pxl` that takes as input a pixel and it returns a string, 
+a single dot if the pixel is a D, the hash/pound sign # if the pixel is H. This is a very 
+simple function. Should have type: `pixel -> string`
+*)
+let string_of_pxl pxl = 
+   if pxl = D
+   then "."
+   else (* H *) "#" 
+   
+(*
+Write a function `string_of_row` that takes as input a row and returns a string 
+consisting of the concatenation of strings corresponding to the pixels in the row.  
+The resulting string should include a newline `"\n"` at the end. Reference solution 
+is 1 line. Should have type: `row -> string`
+*)
+
+(*
+Write a function `string_of_pic` that takes as input a picture and returns a string 
+consisting of the concatenation of the strings produced by each row. The rows already contain 
+newline characters. You do not need to add any extra newlines. Reference solution is 1 line. 
+Should have type: `pic -> string`
+*)
+
+(*
+Write a function `flip_vertical` that takes as input a picture and returns a picture that 
+is a vertical flip of the original one. Reference solution is 1 line.
+Should have type: `pic -> pic`
+*)
+
+(*
+Write a function `flip_horizontal` that takes as input a picture and returns a picture that 
+is a horizontal flip of the original one. Reference solution is 1 line. 
+Should have type: `pic -> pic`
+*)
+
+(*
+Write a function `flip_both` that takes as input a picture and returns a picture that is a 
+"double flip" of the original one, doing both a horizontal and a vertical flip. 
+The order of flips would not matter. Reference solution is 1 line. 
+Should have type: `pic -> pic`
+*)
+
+(*
+Write a function `mirror_vertical` that takes as input a picture and returns a picture that 
+contains two copies of the original picture, one below the other but where the second copy 
+is a mirror image of the first one, as produced by `flip_vertical`. You do not have to use 
+`flip_vertical` along the way. It will have twice the number of rows as the original one. 
+Reference solution is 1 line. 
+Should have type: `pic -> pic`
+*)
+
+(*
+Write a function `mirror_horizontal` that takes as input a picture and returns a picture that contains 
+two copies of the original picture, one next the other horizontally, but where the second copy is a mirror 
+image of the first one as produced for example by `flip_horizontal` (but you will likely not be able to 
+call `flip_horizontal`). It will have twice as long rows as the original one. Reference solution is 1 line. 
+Should have type: `pic -> pic`
+*)
+
+(*
+Write a function `mirror_both` that takes as input a picture and returns a picture that contains 
+four copies of the original picture, arranged in a 2x2 format, and each obtained from the nearby one via a 
+flip in that direction. When applied to the sword picture, it should produce four swords emanating from its center. 
+Reference solution is 1 line. Should have type: `pic -> pic`
+*)
+
+(*
+Write a function `pixelate` that takes as input a function `f` of type `int -> int -> pixel` and two 
+integers `m` and `n`, in that order, and produces a picture of `m` rows and `n` "columns". The pixel at 
+the i-th row and j-th column (in other words the j-th pixel in the i-th row) is determined by the value 
+of the function `f`. Use `tabulate` from earlier (in two places). Reference solution is 1-2 lines. 
+Should have type: `(int -> int -> pixel) -> int -> int -> pic`
+*)
+
+(*
+Write a function `stack_vertical` that takes as input two pictures and places them one atop the other 
+in a vertical fashion. It should raise the exception `IncompatibleDims` if the pictures have different 
+number of columns. Reference solution is 2 lines. 
+Should have type: `pic -> pic -> pic`.
+*)
+
+(*
+Write a function `stack_horizontal` that takes as input two pictures and places them next to 
+each other in a horizontal fashion. It should raise the exception `IncompatibleDims` if the pictures 
+have different number of rows. You should use `List.fold_right2`. You can handle the dimension check 
+by either using the function `dims` provided earlier or letting `List.fold_right2` throw its exception 
+(see documentation) and catching it. Or try them both! Reference solution is 2-4 lines. 
+Should have type: `pic -> pic -> pic`.
+*)
+
+(*
+Write a function `invert` that takes as input a picture and returns the same picture with the 
+two "colors" inverted. Reference solution is 2-4 lines. Should have type: `pic -> pic`
+*)
+
+(*
+Write a function `transpose` that takes as input a picture and returns the 
+result of "transposing" the picture, i.e. turning its rows into columns. This 
+one is short but tricky. The reference solution is 4 lines and uses `List.fold_right`, 
+`List.map` and `List.map2` along with a let binding and a conditional. Start by working out 
+manually in a small example how a recursive implementation might function (but your final 
+solution is not meant to be recursive; this would just help you figure out the kind of work 
+that your `fold_right` would have to do). Should have type: `pic -> pic`
 *)
