2e6194992de8431b62736b8e5f45ff3ea8d18cfb
Evan Miller <Evan Miller>
2016-03-16 15:49:52 -0400

    Finished assignment. Wrote functions. Wrote tests. Tests passed.

--- assignment9tests.rkt
+++ assignment9tests.rkt
@@ -8,33 +8,60 @@
 
 ;; add-nums
 (equal? (add-nums (list)) 0) ;;empty list
-(equal? (add-nums (list 1 2 'a 3)) 6) ;; non-number
+(equal? (add-nums (list 1 2 'a 3)) 6) ;;non-number
 
 ;; length
 (equal? (length (list)) 0) ;; empty list
+(equal? (length (list 1 2 3)) 3) ;; regular list
 
 ;; get-nth
 (with-handlers ([exn:fail? (lambda (exn) (equal? (exn-message exn)
                                                  "negative index"))])
     (get-nth null -2))   ;;negative index
+(equal? (get-nth (list 1 2 3) 2) 3)
+(equal? (get-nth (list 1 2 3) 0) 1)
 
 ;; every-other
 (equal? (every-other (list 1 2 3 4)) (list 1 3)) ;; even length
 (equal? (every-other (list 1 2 3)) (list 1 3))   ;; odd length
+(equal? (every-other (list)) (list)) ;; empty list
 
 ;; map
 (equal? (map (lambda (x) (* x x)) (list 1 2 3))
      (list 1 4 9))       ;; squaring
+(equal? (map (lambda (x) (* x x)) (list))
+     (list))       ;; empty list
+(equal? (map (lambda (x) (+ x x)) (list 1 2 3))
+     (list 2 4 6))       ;; addition
 
 ;; map2
 (equal? (map2 (lambda (x y) (* x y)) (list 1 2 3) (list 2 3 4))
      (list 2 6 12))      ;; multiply
+(equal? (map2 (lambda (x y) (* x y)) (list 1 2 3) (list 2))
+     (list 2))      ;; list 1 smaller
+(equal? (map2 (lambda (x y) (* x y)) (list 1) (list 2 3 4))
+     (list 2))      ;; list 2 smaller
+(equal? (map2 (lambda (x y) (* x y)) (list) (list 2 3 4))
+     (list))      ;; one list empty
 
 ;; filter
 (equal? (filter (lambda (x) (= (modulo x 2) 1))
                (list 1 2 3 4))
      (list 1 3))      ;; odd
+(equal? (filter (lambda (x) (= (modulo x 2) 0))
+               (list 1 2 3 4))
+     (list 2 4))      ;; even
+(equal? (filter (lambda (x) (= (modulo x 2) 0))
+               (list 1 3))
+     (list))      ;; all false
+(equal? (filter (lambda (x) (= (modulo x 2) 0))
+               (list))
+     (list))      ;; empty list
 
 ;; call-all
 (equal? (call-all (list (lambda () 2)))
      (list 2))        ;; one-element
+(equal? (call-all (list (lambda () 2) (lambda () 5)))
+     (list 2 5))        ;; two-element
+(equal? (call-all (list))
+     (list))        ;; zero-elements
